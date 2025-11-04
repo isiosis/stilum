807951508eec19c204dce1dd43b74bf20ec4e04a
Minyahel Haile <minyahel.haile@mhp.com>
2025-11-04 12:30:48 +0100

    add color ansis

---
+++ lib/src/main/kotlin/com/github/isiosis/Colors.kt
@@ -0,0 +1,17 @@
+package com.github.isiosis
+
+const val escapeCharacter = "\u001b["
+
+enum class Colors (
+    val ansiCode: String
+) {
+    BLACK(ansiCode = "${escapeCharacter}30m"),
+    RED(ansiCode = "${escapeCharacter}31m"),
+    GREEN(ansiCode = "${escapeCharacter}32m"),
+    YELLOW(ansiCode = "${escapeCharacter}33m"),
+    BLUE(ansiCode = "${escapeCharacter}34m"),
+    MAGNETA(ansiCode = "${escapeCharacter}35m"),
+    CYAN(ansiCode = "${escapeCharacter}36m"),
+    WHITE(ansiCode = "${escapeCharacter}37m")
+
+}
