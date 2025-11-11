fe6228f78abd01c7db5003e082efba58323ad3f8
isiosis <se.minyahel.fekadu@gmail.com>
2025-11-11 14:54:01 +0100

    fix location and type IoHandler

--- lib/src/main/kotlin/io/github/isiosis/stilum/ioHandler/IoHandler.kt
+++ lib/src/main/kotlin/io/github/isiosis/stilum/ioHandler/IoHandler.kt
@@ -4,12 +4,10 @@ import io.github.isiosis.stilum.Colors
 
 class IoHandler {
     companion object {
-        @JvmStatic
         fun prepareStyledString(message: String, color: Colors): String {
             return "${color.ansiCode}${message}${Colors.DEFAULT.ansiCode}"
         }
 
-        @JvmStatic
         fun printWithStandardLibrary(message: String) {
             println(message)
         }
